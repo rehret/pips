3215784d09f6783047ee95c1f420e04525a9381a
Randy Ehret <randyehret@gmail.com>
2018-02-14 09:41:54 -0600

    Add swagger-ui to api

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,19 @@ import "reflect-metadata";
 
 import * as Nconf from "nconf";
 import { Log } from "./log";
-import { createKoaServer, useContainer } from "routing-controllers";
+import * as Koa from "koa";
+import { useKoaServer, useContainer } from "routing-controllers";
 import { Container } from "./config/inversify-config";
+import { ui } from "swagger2-koa";
 
 useContainer(Container);
 
-let app = createKoaServer({
+const app = new Koa();
+
+const swaggerDoc = require("../swagger.json");
+app.use(ui(swaggerDoc, "/swagger"));
+
+useKoaServer(app, {
     controllers: [`${__dirname}/controllers/*.[tj]s`],
     middlewares: [`${__dirname}/middleware/*.[tj]s`],
     defaultErrorHandler: false
